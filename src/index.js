66413e4c57c38f6f7df279552805258b7a996340
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-03-08 15:16:17 -0500

    final working commit

--- src/index.js
+++ src/index.js
@@ -5,18 +5,5 @@ import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-class AllTheRoutes extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/callback/" component={App} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
-
-ReactDOM.render(<AllTheRoutes />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
