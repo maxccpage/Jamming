7112a7b8ad1cf646403edc3fd497063148c461f7
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-03-08 13:59:09 -0500

    I Broke my life

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+// import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-const allTheRoutes = () => {
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={App} />
-      <Route path="/callback" component={App} />
-      <Route render={() => 'Not working hoe'} />
-    </Switch>
-  </BrowserRouter>;
-};
+// const allTheRoutes = () => {
+//   <BrowserRouter>
+//     <Switch>
+//       <Route path="/" component={App} />
+//       <Route path="/callback" component={App} />
+//       <Route render={() => 'Not working hoe'} />
+//     </Switch>
+//   </BrowserRouter>;
+// };
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
