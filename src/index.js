7d847a99e583b58f4a1c724a4569b68c9aaba3f9
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-03-08 14:21:59 -0500

    trying again

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-const allTheRoutes = () => {
-  <BrowserRouter>
-    <Switch>
-      <Route path="*" render={() => <Redirect to="/" />} />
-    </Switch>
-  </BrowserRouter>;
-};
+class allTheRoutes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={App} />
+          <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 ReactDOM.render(<allTheRoutes />, document.getElementById('root'));
 registerServiceWorker();
